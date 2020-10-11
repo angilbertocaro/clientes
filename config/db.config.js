4bf6a99d786ac25f07a0d199528ec66b9b4f0af1
Angilberto Caro <acaro@powersoftec.com>
2020-10-11 03:57:54 -0600

    se agrego base de datos a heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "DBClientes"
+    HOST: "nwhazdrp7hdpd4a4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    USER: "c0bpzcxqlzs7x6am",
+    PASSWORD: "d5nq42zgme4m26zz",
+    DB: "hh3wfgg2n7gsvejy"
 };
